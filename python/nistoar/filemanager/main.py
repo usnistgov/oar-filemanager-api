708e59a8ef349bb01002639e8a990d08971c6f22
elmiomar <omarilias.elmimouni@nist.gov>
2022-07-18 09:26:41 -0400

    add main guard

--- python/nistoar/filemanager/main.py
+++ python/nistoar/filemanager/main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Service": "NextCloud File Manager API", "Status": "Running"}
+
+
+def main():
+    import uvicorn
+
+    uvicorn.run("nistoar.filemanager.main:app", host="0.0.0.0", port=8085, reload=True, debug=True)
+
+
+if __name__ == "__main__":
+    main()
