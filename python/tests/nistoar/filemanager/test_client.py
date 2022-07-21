da286ad34c928d681805bd181501b3c73990e594
elmiomar <omarilias.elmimouni@nist.gov>
2022-07-21 13:53:06 -0400

    refactor

--- python/tests/nistoar/filemanager/test_client.py
+++ python/tests/nistoar/filemanager/test_client.py
@@ -5,33 +5,6 @@ from nistoar.filemanager.client import WebDavClient
 from tests.nistoar.filemanager.webdav_server import WebDavTestServer
 
 
-def test_construct_url():
-    client = BaseClient(base_url="https://nextcloud.com/test/client", api_url="sharing")
-    full_url = client.construct_url(extra_url="files")
-    expected_url = "https://nextcloud.com/test/client/sharing/files"
-    assert full_url == expected_url
-
-
-@responses.activate
-def test_get():
-    responses.add(
-        **{
-            "method": responses.GET,
-            "url": "http://example.com/api",
-            "body": '{"status": "up"}',
-            "status": 200,
-            "content_type": "application/json",
-        }
-    )
-
-    client = BaseClient(base_url="http://example.com", api_url="api")
-    response = client.get()
-
-    assert response.json() == {"status": "up"}
-    assert client.construct_url() == "http://example.com/api"
-    assert response.status_code == 200
-
-
 class ClientTest(unittest.TestCase):
     def setUp(self):
         self.client = BaseClient(base_url="http://127.0.0.1:8880")
@@ -39,10 +12,38 @@ class ClientTest(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_construct_url(self):
+        client = BaseClient(
+            base_url="https://nextcloud.com/test/client", api_url="sharing"
+        )
+        full_url = client.construct_url(extra_url="files")
+        expected_url = "https://nextcloud.com/test/client/sharing/files"
+        self.assertTrue(full_url == expected_url)
+
+    @responses.activate
+    def test_get_using_responses_lib(self):
+        responses.add(
+            **{
+                "method": responses.GET,
+                "url": "http://example.com/api",
+                "body": '{"status": "up"}',
+                "status": 200,
+                "content_type": "application/json",
+            }
+        )
+
+        client = BaseClient(base_url="http://example.com", api_url="api")
+        response = client.get()
+
+        self.assertTrue(response.json() == {"status": "up"})
+        self.assertTrue(client.construct_url() == "http://example.com/api")
+        self.assertTrue(response.status_code == 200)
+
     def test_get(self):
         with WebDavTestServer(blocking=False) as server:
             response = self.client.get()
-            assert response.status_code == 200
+            self.assertTrue(response.status_code == 200)
+
 
 class WebDavClientTest(unittest.TestCase):
     def setUp(self):
@@ -54,7 +55,8 @@ class WebDavClientTest(unittest.TestCase):
     def test_mkcol(self):
         with WebDavTestServer(blocking=False) as server:
             response = self.webdav_client.make_collection(url="my_collection")
-            assert response.status_code == 201
+            self.assertTrue(response.status_code == 201)
+
 
 if __name__ == "__main__":
     unittest.main()
