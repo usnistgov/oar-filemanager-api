27b24ac41637aa6216396b68fbd758e57a6bf07e
elmiomar <elmimouni.o.i@gmail.com>
2022-07-21 06:02:33 -0400

    clean + build_url util function

--- python/nistoar/filemanager/util.py
+++ python/nistoar/filemanager/util.py
@@ -3,9 +3,10 @@ import os
 import sys
 import tempfile
 from loguru import logger
+from loguru._defaults import LOGURU_FORMAT
 
 
-def disable_fastapi_logging():
+def disable_uvicorn_logging():
     uvicorn_error = logging.getLogger("uvicorn.error")
     uvicorn_error.disabled = True
     uvicorn_access = logging.getLogger("uvicorn.access")
@@ -15,10 +16,12 @@ def disable_fastapi_logging():
 NONE_LOG_LEVEL = "NONE"
 DEFAULT_LOG_LEVEL = "DEBUG"
 
-LOGURU_DEFAULT_FORMAT = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
-
+LONG_FORMAT = LOGURU_FORMAT
+SHORT_FORMAT = "<yellow>{time:YYYY-MM-DD at HH:mm:ss}</yellow> | <level>[{level}]</level> {message}"
 
 def initialize_logger(name, log_level=DEFAULT_LOG_LEVEL):
+    """Initiialize Logaru logger."""
+
     logger.remove()
 
     if log_level == NONE_LOG_LEVEL:
@@ -26,15 +29,17 @@ def initialize_logger(name, log_level=DEFAULT_LOG_LEVEL):
         return
 
     if log_level == "DEBUG":
-        log_format = "<yellow>{time:YYYY-MM-DD at HH:mm:ss}</yellow> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+        log_format = LONG_FORMAT
     else:
-        log_format = "<yellow>{time:YYYY-MM-DD at HH:mm:ss}</yellow> | <level>[{level}]</level> {message}"
+        log_format = SHORT_FORMAT
 
     logger.add(sys.stdout, colorize=True, format=log_format, level=log_level)
     logger.enable(name)
 
 
 def create_test_folder(name="testfolder"):
+    """Create a temporary folder for testing purposes."""
+
     sys_tmp = tempfile.gettempdir()
     test_tmp = os.path.join(sys_tmp, name)
     if not os.path.exists(test_tmp):
@@ -43,5 +48,13 @@ def create_test_folder(name="testfolder"):
     logger.debug(f"created temporary folder {tmp_dir}")
     return tmp_dir
 
+def build_url(host, port, endpoint="", scheme="http"):
+    """Build url."""
 
-disable_fastapi_logging()
+    url = f"{scheme}://{host}"
+    if port is not None:
+        url += f":{port}"
+    url += f"/{endpoint}"
+    return url 
+
+disable_uvicorn_logging()
